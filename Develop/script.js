f31902e94db865a31034029f9748e977c3bed228
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-01-03 18:36:20 -0800

    added function to generate password

--- Develop/script.js
+++ Develop/script.js
@@ -18,12 +18,28 @@ function writePassword() {
    ) { 
      window.alert("Must select minimum 8 characters, but no more than 128 characters long.")
 
+  } else{
+    let upperCaseCheck = window.confirm("Do you want to select Upper Case letters in your password?")
+    let lowerCaseCheck = window.confirm("Do you want to select Lower Case letters in your password?")
+    let numericCaseCheck = window.confirm("Do you want to select a Number in your password?")
+    let specialCharactersCheck = window.confirm("Do you want to add a special character in your password?")
+
+
+    var password = generatePassword(passwordLengthPrompt, upperCaseCheck, lowerCaseCheck, numericCaseCheck, specialCharactersCheck);
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
   }
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+}
 
-  passwordText.value = password;
+function generatePassword(passwordLengthPrompt, upperCaseCheck, lowerCaseCheck, numericCaseCheck, specialCharactersCheck) {
+  var upperCasePassword = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerCasePassword = "abcdefghijklmnopqrstuvwxyz";
+  var numericCaseCPassword = "0123456789";
+  var specialCharactersPassword = "#$!@!$^&";
+  let password = "";
 
+
+  
 }
 
 
