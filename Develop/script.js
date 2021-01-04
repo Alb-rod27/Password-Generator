a5ed50f8adeba226be0f105859df6e8473a9f478
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-01-03 17:18:53 -0800

    added window prompts

--- Develop/script.js
+++ Develop/script.js
@@ -3,11 +3,22 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-let values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
-let password = "";
+
 
 // Write password to the #password input
 function writePassword() {
+  passwordLengthPrompt = window.prompt(
+   "Enter password length"
+  );
+
+  if (
+    passwordLengthPrompt === null || 
+    passwordLengthPrompt  < 8 ||
+    passwordLengthPrompt  > 128
+   ) { 
+     window.alert("Must select minimum 8 characters, but no more than 128 characters long.")
+
+  }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -15,5 +26,8 @@ function writePassword() {
 
 }
 
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
